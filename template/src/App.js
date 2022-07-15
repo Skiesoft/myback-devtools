9999b6ef974ff7ceeec0bdba67546c429ca7a42d
jw910731 <jw910731@gmail.com>
2022-07-15 14:35:04 +0800

    [DevEnv] Enforce example code in template

--- template/src/App.js
+++ template/src/App.js
@@ -1,20 +1,52 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import SDK from 'myback-sdk';
 
-function App (){
+function App() {
   const sdk = new SDK();
+  const [collections, setCollections] = useState();
+  const [workingCollection, setWorkingCollection] = useState();
+  const [objects, setObjects] = useState();
 
-  const fetchData = async () => {
-    console.log(await sdk.getResources());
+  const fetchCollections = async () => {
+    try {
+      let response = await sdk.getResources();
+      response = await response[0].getCollections();
+      setCollections(response);
+    } catch (error) {
+      console.log(error);
+    }
   };
   useEffect(() => {
-    fetchData();
+    fetchCollections();
   }, []);
 
-  return(
+  const fetchData = async () => {
+    try {
+      const response = await workingCollection?.getPage();
+      setObjects(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fetchData();
+  }, [workingCollection]);
+
+  return (
     <div className="App">
-      <h1> Hello, World! </h1>
+      {collections?.map((collection, index) => <h1 key={index}>{collection.collectionId}</h1>)}
+      <input type='text' onChange={e => setWorkingCollection(collections?.find(v => v.collectionId == e.target.value))}></input>
+      {objects?.map((object, index) => <div key={index}>
+        {((obj) => {
+          let ret = [];
+          let i = 0;
+          for (const [key, value] of Object.entries(obj.properties)) {
+            ret.push(<div key={i++}>{value}</div>)
+          }
+          return ret;
+        })(object)}
+      </div>)}
     </div>
   );
 }
